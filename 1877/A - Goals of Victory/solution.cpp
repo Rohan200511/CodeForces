0de29518014dfb4e05f4f24f7544dd33572135c6
Rohan <rohangupta112005@gmail.com>
2026-04-22 16:30:52 +0530

    Add Goals of Victory [A] from Codeforces

---
+++ 1877/A - Goals of Victory/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        int n;
+        cin >> n;
+ 
+        int sum = 0;
+ 
+        n = n-1;
+ 
+        while(n--){
+            int a;
+            cin >> a;
+            sum += a; 
+        }
+ 
+        cout << -(sum) << "
+";
+    }
+    return 0;
+}
